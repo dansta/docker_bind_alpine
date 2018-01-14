e70ad191c158ed27063ec277cc0b4c3abdb43b79
dansta <danne@kvantmekanik.se>
2018-01-14 12:25:30 +0100

    Lunchbreak, committing all my work until further notice

---
+++ bind/files/wrapper.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -x
+
+/usr/bin/wget --no-check-certificate -O BIND_ROOT_SERVERS https://www.internic.net/domain/named.cache
+/usr/sbin/named -n 4 -U 4 -p BIND_PORT -u BIND_EFFECTIVE_USER -c BIND_CONF -d4
