43978322d30433d76a9845933ad475ea5004ee9b
dansta <danne@kvantmekanik.se>
2018-01-14 16:23:42 +0100

    This one runs

--- bind/files/wrapper.sh
+++ bind/files/wrapper.sh
@@ -2,4 +2,4 @@
 set -x
 
 /usr/bin/wget --no-check-certificate -O BIND_ROOT_SERVERS https://www.internic.net/domain/named.cache
-/usr/sbin/named -n 4 -U 4 -p BIND_PORT -u BIND_EFFECTIVE_USER -c BIND_CONF -d4
+/usr/sbin/named -4 -n 4 -U 4 -p BIND_PORT -u BIND_EFFECTIVE_USER -c BIND_CONF -d2
